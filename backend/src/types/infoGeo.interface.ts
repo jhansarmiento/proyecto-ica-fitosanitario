dbd174edd66397d1a00ccde6125d8e3d208f1461
jhansarmiento <jhancsn1@gmail.com>
2026-05-28 17:25:27 -0500

    Arregla el problema N/D al listar los predios

--- backend/src/types/infoGeo.interface.ts
+++ backend/src/types/infoGeo.interface.ts
@@ -1,17 +1,17 @@
 // ───  Interfaces de Contrato para el Catálogo Geográfico ───────────────────
 export interface IDepartamento {
     id_departamento: string;
-    nombre_departamento?: string;
+    nombre?: string;
 }
 
 export interface IMunicipio {
     id_municipio: string;
-    nombre_municipio?: string;
+    nombre?: string;
     departamento?: IDepartamento; // Relación anidada
 }
 
 export interface IVeredaGeografica {
     id_vereda: string;
-    nombre_vereda?: string;
+    nombre?: string;
     municipio?: IMunicipio; // Relación anidada
 }
